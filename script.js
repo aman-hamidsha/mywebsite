b90873b32c93b617182b9c18f3e2be0c2829c8b8
Aman Hamidsha <155252741+aman-hamidsha@users.noreply.github.com>
2026-08-05 09:01:09 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const b=document.body,t=document.getElementById("toggle");
 t.onclick=()=>{
     b.classList.toggle("light");
     b.classList.toggle("dark");
-    t.textContent=b.classList.contains("dark")?"☀️":"🌙";
+    t.textContent=b.classList.contains("dark")?"light":"dark";
 };
